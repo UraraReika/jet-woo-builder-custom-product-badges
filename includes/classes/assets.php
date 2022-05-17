f111bc434c769264477c516401f39ec3badf3fb3
Alexandr Rudy <aoikomete@gmail.com>
2022-05-17 19:00:31 +0300

    UPD: limit css enqueue

--- includes/classes/assets.php
+++ includes/classes/assets.php
@@ -21,6 +21,13 @@ class Assets {
 	 * @access public
 	 */
 	public function enqueue_admin_assets() {
+
+		$current_screen = get_current_screen();
+
+		if ( ( ! isset( $_GET['subpage'] ) || 'jwb-custom-product-badges-general-settings' !== $_GET['subpage'] ) && 'product' !== $current_screen->post_type ) {
+			return;
+		}
+
 		wp_enqueue_style(
 			'jwbcpb-admin-styles',
 			JWB_CUSTOM_PRODUCT_BUDGES_URL . 'assets/css/admin/styles.css',

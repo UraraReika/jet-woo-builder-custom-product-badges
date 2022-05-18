517c20aa7b8d39cebf8f948df2beec9a06a7b854
Alexandr Rudy <aoikomete@gmail.com>
2022-05-18 16:34:54 +0300

    SYS: bump version to 1.1.0

--- jet-woo-builder-custom-product-badges.php
+++ jet-woo-builder-custom-product-badges.php
@@ -3,7 +3,7 @@
  * Plugin Name: JetWooBuilder - Custom Product Badges
  * Plugin URI: https://github.com/UraraReika/jet-woo-builder-custom-product-badges
  * Description: Custom Product Badges has it all: create and manage products badges, output in multiple widgets, style badges separately and more.
- * Version:     1.0.0
+ * Version:     1.1.0
  * Author:      Crocoblock
  * Author URI:  https://crocoblock.com/
  * Text Domain: jwb-custom-product-badges
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'JWB_CUSTOM_PRODUCT_BUDGES_VERSION', '1.0.0' );
+define( 'JWB_CUSTOM_PRODUCT_BUDGES_VERSION', '1.1.0' );
 
 define( 'JWB_CUSTOM_PRODUCT_BUDGES__FILE__', __FILE__ );
 define( 'JWB_CUSTOM_PRODUCT_BUDGES_PLUGIN_BASE', plugin_basename( JWB_CUSTOM_PRODUCT_BUDGES__FILE__ ) );

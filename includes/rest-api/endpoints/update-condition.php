6631b35be6ba67913daaab284161f244c4b1f4d3
Alexandr Rudy <aoikomete@gmail.com>
2023-01-20 12:03:23 +0200

    Update update-condition.php

--- includes/rest-api/endpoints/update-condition.php
+++ includes/rest-api/endpoints/update-condition.php
@@ -2,6 +2,8 @@
 
 namespace JWB_CPB\Endpoints;
 
+use Automattic\WooCommerce\Blocks\Package;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
@@ -20,12 +22,93 @@ class Update_Condition extends Base {
 
 		parent::callback( $request );
 
+		$data = $request->get_params();
+
+		foreach ( $data['displayConditions'] as $condition ) {
+			$this->handle_products_badges_meta( $condition );
+		}
+
 		return rest_ensure_response( [
 			'status'  => 'success',
 			'message' => __( 'Condition has been updated.', 'jwb-custom-product-badges' ),
 		] );
 
+	}
 
+	/**
+	 * Handle products badge meta.
+	 *
+	 * Handle products badges meta for different types of parameter.
+	 *
+	 * @since  1.3.0
+	 * @access public
+	 *
+	 * @param array $condition Currently handling condition.
+	 *
+	 * @return void
+	 */
+	public function handle_products_badges_meta( $condition ) {
+
+		$args = [
+			'post_type'   => 'product',
+			'numberposts' => -1,
+		];
+
+		switch ( $condition['parameter'] ) {
+			case 'products':
+				$condition_products = get_posts( array_merge( $args, [ 'include' => $condition['value'] ] ) );
+				$products           = get_posts( array_merge( $args, [ 'exclude' => $condition['value'] ] ) );
+				$include            = 'equal' === $condition['operator'];
+
+				$this->update_products_badges_meta( $condition_products, $condition, $include );
+				$this->update_products_badges_meta( $products, $condition, ! $include );
+
+				break;
+
+			default:
+				break;
+		}
+
+	}
+
+	/**
+	 * Update products badges meta.
+	 *
+	 * Updates products badges meta depending on operator.
+	 *
+	 * @since  1.3.0
+	 * @access public
+	 *
+	 * @param array $products  List of products.
+	 * @param array $condition Currently handling condition.
+	 * @param bool  $include   Update status.
+	 *
+	 * @return void
+	 */
+	public function update_products_badges_meta( $products, $condition, $include ) {
+		foreach ( $products as $product ) {
+			$badge_meta = get_post_meta( $product->ID, '_jet_woo_builder_badges', true );
+
+			if ( $include ) {
+				if ( empty( $badge_meta ) ) {
+					$badge_meta = $condition['badges'];
+				} else {
+					$badge_meta = array_unique( array_merge( $badge_meta, $condition['badges'] ) );
+				}
+			} else {
+				if ( empty( $badge_meta ) ) {
+					continue;
+				}
+
+				foreach ( $badge_meta as $key => $value ) {
+					if ( in_array( $value, $condition['badges'] ) ) {
+						unset( $badge_meta[ $key ] );
+					}
+				}
+			}
+
+			update_post_meta( $product->ID, '_jet_woo_builder_badges', $badge_meta );
+		}
 	}
 
 }
